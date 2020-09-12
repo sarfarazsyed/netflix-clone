aef5b0b81928b76a5eb8a4191e3ac61f990f5329
Sarfaraz Ahammed <syedsarfarazahammed@gmail.com>
2020-09-12 18:18:30 +0530

    changes

---
+++ build/precache-manifest.02bc05e5b61633bba3d51aae5a31f50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6758fc2beaa489663238fe913929a0ae",
+    "url": "/netflixClone/index.html"
+  },
+  {
+    "revision": "7bb221a2b9fb2979af31",
+    "url": "/netflixClone/static/css/main.a720e5cc.chunk.css"
+  },
+  {
+    "revision": "986b47ec474e824a5129",
+    "url": "/netflixClone/static/js/2.dfb2f2e3.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflixClone/static/js/2.dfb2f2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb221a2b9fb2979af31",
+    "url": "/netflixClone/static/js/main.f59b0640.chunk.js"
+  },
+  {
+    "revision": "c2ebfca4b867fee391cd",
+    "url": "/netflixClone/static/js/runtime-main.003b2803.js"
+  }
+]);
